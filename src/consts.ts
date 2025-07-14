518ed59c22921388c2871fd068d247566609b521
suzuki <suzuki.sayaka810@gmail.com>
2025-07-14 16:46:36 +0900

    基本デザイン変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'THUNDERS FC - 公式サイト';
+export const SITE_DESCRIPTION = 'THUNDERS FCの公式ウェブサイト。最新ニュース、試合結果、選手情報、チケット情報をお届けします。';
+export const TEAM_NAME = 'THUNDERS FC';
