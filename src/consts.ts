b455cdeeef00f66897c4930320fe5106f216527b
suzuki <suzuki.sayaka810@gmail.com>
2025-07-14 16:50:25 +0900

    チーム名変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'THUNDERS FC - 公式サイト';
-export const SITE_DESCRIPTION = 'THUNDERS FCの公式ウェブサイト。最新ニュース、試合結果、選手情報、チケット情報をお届けします。';
-export const TEAM_NAME = 'THUNDERS FC';
+export const SITE_TITLE = 'インフィニティ - 公式サイト';
+export const SITE_DESCRIPTION = 'インフィニティの公式ウェブサイト。最新ニュース、試合結果、選手情報、チケット情報をお届けします。';
+export const TEAM_NAME = 'インフィニティ';
